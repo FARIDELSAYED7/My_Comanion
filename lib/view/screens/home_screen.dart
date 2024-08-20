921cb7d6fd23e599be3da419dceb7bb832666d01
FARID_ELSAYED7 <151828384+FARIDELSAYED7@users.noreply.github.com>
2024-08-20 20:49:51 +0300

    LastChange

--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -8,8 +8,15 @@ const String _goodMorning = "Good Morning";
 const String _goodAfternoon = "Good Afternoon";
 const String _goodEvening = "Good Evening";
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _todoBox = Hive.box<Todo>('todos');
 
   @override
   Widget build(BuildContext context) {
@@ -21,13 +28,7 @@ class HomeScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              TimeOfDay.now().hour < 12
-                  ? text(
-                      title: _goodMorning,
-                    )
-                  : TimeOfDay.now().hour < 18
-                      ? text(title: _goodAfternoon)
-                      : text(title: _goodEvening),
+              _buildGreeting(),
               const Text(
                 "How are you feeling today?",
                 style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
@@ -84,68 +85,59 @@ class HomeScreen extends StatelessWidget {
                 height: 10,
               ),
               Expanded(
-                child: FutureBuilder(
-                  future: Hive.openBox<Todo>('todos'),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.done) {
-                      if (snapshot.hasData) {
-                        final box = snapshot.data as Box<Todo>;
-                        final todos = box.values.toList().cast<Todo>();
-                        return ListView.builder(
-                          itemCount: todos.length,
-                          itemBuilder: (context, index) {
-                            final todo = todos[index];
-                            return ListTile(
-                              title: Text(
-                                todo.title,
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    color: todo.isCompleted
-                                        ? Colors.grey
-                                        : Colors.black,
-                                    fontWeight: FontWeight.w600,
-                                    decoration: todo.isCompleted
-                                        ? TextDecoration.lineThrough
-                                        : null),
+                child: ValueListenableBuilder(
+                  valueListenable: _todoBox.listenable(),
+                  builder: (context, _, __) {
+                    final todos = _todoBox.values.toList().cast<Todo>();
+                    return ListView.builder(
+                      itemCount: todos.length,
+                      itemBuilder: (context, index) {
+                        final todo = todos[index];
+                        return ListTile(
+                          title: Text(
+                            todo.title,
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: todo.isCompleted
+                                    ? Colors.grey
+                                    : Colors.black,
+                                fontWeight: FontWeight.w600,
+                                decoration: todo.isCompleted
+                                    ? TextDecoration.lineThrough
+                                    : null),
+                          ),
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Checkbox(
+                                value: todo.isCompleted,
+                                onChanged: (value) {
+                                  setState(() {
+                                    todo.isCompleted = value!;
+                                    _todoBox.putAt(index, todo);
+                                  });
+                                },
                               ),
-                              trailing: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Checkbox(
-                                    value: todo.isCompleted,
-                                    onChanged: (value) {
-                                      todo.isCompleted = value!;
-                                      box.putAt(index, todo);
-                                    },
-                                  ),
-                                  IconButton(
-                                    icon: const Icon(Icons.delete),
-                                    onPressed: () {
-                                      box.deleteAt(index);
-                                    },
-                                  ),
-                                  SizedBox(
-                                    width: 5,
-                                  ),
-                                  IconButton(
-                                    icon: const Icon(Icons.edit),
-                                    onPressed: () => _editTodo(context, todo),
-                                  ),
-                                ],
+                              IconButton(
+                                icon: const Icon(Icons.delete),
+                                onPressed: () {
+                                  setState(() {
+                                    _todoBox.deleteAt(index);
+                                  });
+                                },
                               ),
-                            );
-                          },
+                              SizedBox(
+                                width: 5,
+                              ),
+                              IconButton(
+                                icon: const Icon(Icons.edit),
+                                onPressed: () => _editTodo(context, todo),
+                              ),
+                            ],
+                          ),
                         );
-                      } else {
-                        return const Center(
-                          child: Text('No data'),
-                        );
-                      }
-                    } else {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
+                      },
+                    );
                   },
                 ),
               ),
@@ -155,29 +147,40 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
-}
 
-void _addNewTodo(BuildContext context) async {
-  final titleController = TextEditingController();
-  showDialog(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
-        title: const Text('Add Todo'),
-        content: TextField(
-          controller: titleController,
-          decoration: const InputDecoration(hintText: 'Enter todo'),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: const Text('Cancel'),
+  Widget _buildGreeting() {
+    final now = DateTime.now();
+    final hour = now.hour;
+    final greeting = hour < 12
+        ? _goodMorning
+        : hour < 18
+            ? _goodAfternoon
+            : _goodEvening;
+    return text(title: greeting);
+  }
+
+  void _addNewTodo(BuildContext context) async {
+    final titleController = TextEditingController();
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Add Todo'),
+          content: TextField(
+            controller: titleController,
+            decoration: const InputDecoration(hintText: 'Enter todo'),
           ),
-          TextButton(
-            onPressed: () {
-              final todo = Todo(title: titleController.text, mod: null);
-              titleController.text == ""
-                  ? showDialog(
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                final todo = Todo(title: titleController.text, mod: null);
+                setState(() {
+                  if (titleController.text == "") {
+                    showDialog(
                       context: context,
                       builder: (context) {
                         return AlertDialog(
@@ -198,45 +201,51 @@ void _addNewTodo(BuildContext context) async {
                             ),
                           ],
                         );
-                      })
-                  : Hive.box<Todo>('todos').add(todo);
-              Navigator.pop(context);
-            },
-            child: const Text('Add'),
-          ),
-        ],
-      );
-    },
-  );
-}
+                      });
+                  } else {
+                    _todoBox.add(todo);
+                  }
+                });
+                Navigator.pop(context);
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
-void _editTodo(BuildContext context, Todo todo) async {
-  final titleController = TextEditingController(text: todo.title);
-  showDialog(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
-        title: const Text('Edit Todo'),
-        content: TextField(
-          controller: titleController,
-          decoration: const InputDecoration(hintText: 'Edit todo'),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: const Text('Cancel'),
+  void _editTodo(BuildContext context, Todo todo) async {
+    final titleController = TextEditingController(text: todo.title);
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Edit Todo'),
+          content: TextField(
+            controller: titleController,
+            decoration: const InputDecoration(hintText: 'Edit todo'),
           ),
-          TextButton(
-            onPressed: () {
-              todo.title = titleController.text;
-              todo.save();
-              Navigator.pop(context);
-            },
-            child: const Text('Save'),
-          ),
-        ],
-      );
-    },
-  );
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  todo.title = titleController.text;
+                  todo.save();
+                });
+                Navigator.pop(context);
+              },
+              child: const Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
 
